1fd717f8fa1ef2e5d5ec63dbaa243a7a1a264b74
Greg Gancarz <propterprivatum@tuta.io>
2019-04-05 12:00:46 -0500

    clicking player buttons instantiates players

--- js/app.js
+++ js/app.js
@@ -13,29 +13,26 @@
 // when all questions have been answered, users are presented their scores 
 // and a button that takes them back to the start menu screen.
 
+
+
 class Player {
 	constructor() {
 		this.points = 0;
 	}
-	pickAnswer() {
-		console.log('answer picked');
-	}
-    initiateGame() {
-        console.log('player count picked');
+    pickAnswer() {
+        console.log('answer picked');
     }
-    returnToMain() {
-        //refresh page
-    }
+	
 }
 
 class Question {
-    constructor() {
-        this.question = '';
-        a = '',
-        b = '',
-        c = '',
-        d = '',
-        correct = 'a'
+    constructor(question, a, b, c, d, correct) {
+        this.question = question;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.correct = correct
     }
 }   
 
@@ -44,20 +41,44 @@ const game = {
     questionsAsked: 0,
     victor: '', // player with high points
     status: '', // start-menu, quiz-active, round-over
+
     begin1P() {
         console.log('1P game has begun');
+        const player1 = new Player();
+        console.log(player1);
     },
     begin2P() {
         console.log('2P game has begun');
+        const player1 = new Player();
+        const player2 = new Player();
+        console.log(player1);
+        console.log(player2);
     },
     begin3P() {
         console.log('3P game has begun');
+        const player1 = new Player();
+        const player2 = new Player();
+        const player3 = new Player();
+        console.log(player1);
+        console.log(player2);
+        console.log(player3);
     },
     begin4P() {
         console.log('4P game has begun');
+        const player1 = new Player();
+        const player2 = new Player();
+        const player3 = new Player();
+        const player4 = new Player();
+        console.log(player1);
+        console.log(player2);
+        console.log(player3);
+        console.log(player4);
     },
     pickVictor() {
         console.log('high score player recognized');
+    },
+    returnToMain() {
+        location.reload();
     }
 }
     
