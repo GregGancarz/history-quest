44aaf8a01eb49a8b581862c92fc07afcf0d577be
Greg Gancarz <propterprivatum@tuta.io>
2019-04-05 13:18:29 -0500

    wrote out 15 questions

--- js/app.js
+++ js/app.js
@@ -36,26 +36,25 @@ class Question {
     }
 }   
 
+
+
 const game = {
     activePlayer: '',
     questionsAsked: 0,
     victor: '', // player with high points
     status: '', // start-menu, quiz-active, round-over
-
+    questArr: [],
     begin1P() {
-        console.log('1P game has begun');
         const player1 = new Player();
         console.log(player1);
     },
     begin2P() {
-        console.log('2P game has begun');
         const player1 = new Player();
         const player2 = new Player();
         console.log(player1);
         console.log(player2);
     },
     begin3P() {
-        console.log('3P game has begun');
         const player1 = new Player();
         const player2 = new Player();
         const player3 = new Player();
@@ -64,7 +63,6 @@ const game = {
         console.log(player3);
     },
     begin4P() {
-        console.log('4P game has begun');
         const player1 = new Player();
         const player2 = new Player();
         const player3 = new Player();
@@ -79,19 +77,56 @@ const game = {
     },
     returnToMain() {
         location.reload();
+    },
+    genQuestions() {
+        const q1 = new Question('Catherine the Great ruled what country?', 'Russia', 'France', 'Germany', 'England', 'a');
+        this.questArr.push(q1);
+        const q2 = new Question('During which year did Christopher Columbus first arrive in the Americas?', '1492', '1503', '1483', '1514', 'a');
+        this.questArr.push(q2);
+        const q3 = new Question('The Islamic conquest of Spain was initiated by which caliphate?', 'Umayyad', 'Rashidun', 'Abbasid', 'Ottoman', 'a');
+        this.questArr.push(q3);
+        const q4 = new Question('Which of the following men is known as the "father of Texas"?', 'Stephen Austin', 'Sam Houston', 'Alexander Dalls', 'Davey Crockett', 'a');
+        this.questArr.push(q4);
+        const q5 = new Question('Julius Caesar was assassinated in what year?', '10 AD', '44 BC', '112 AD', '34 BC', 'b');
+        this.questArr.push(q5);
+        const q6 = new Question('Much of what is now Illinois was originally explored and settled by which Imperial Power?', 'England', 'France', 'Spain', 'Portugal', 'b');
+        this.questArr.push(q6);
+        const q7 = new Question('The Great War spanned which years?', '1909 - 1911', '1914-1918', '1904-1912', '1939-1945', 'b');
+        this.questArr.push(q7);
+        const q8 = new Question('King Henry VIII ruled which Kingdom?', 'France', 'England', 'Spain', 'Holland', 'b');
+        this.questArr.push(q8);
+        const q9 = new Question('The Chicago fire occurred in what year?', '1903', '1899', '1871', '1798', 'c');
+        this.questArr.push(q9);
+        const q10 = new Question('Thomas Jefferson died on the same day in the same year as which other founding father?', 'George Washington', 'Ben Frankin', 'John Adams', 'James Madison', 'c');
+        this.questArr.push(q10);
+        const q11 = new Question('Puerto Rico gained independence from Spain during which president\'s term?', 'Theodore Roosevelt', 'William Taft', 'William McKinley', 'Woodrow Wilson', 'c');
+        this.questArr.push(q11);
+        const q12= new Question('Dom Pedro I was the founder of what nation?', 'Mexico', 'Bolivia', 'Brazil', 'Chile', 'c');
+        this.questArr.push(q12);
+        const q13 = new Question('Ireland gained independence from Great Britain in what year?', '1590', '1686', '1891', '1921', 'd');
+        this.questArr.push(q13);
+        const q14 = new Question('Which French King lost his head at the climax of the French Revolution', 'Charles X', 'Napoleon I', 'Louis XIV', 'Louis XVI', 'd');
+        this.questArr.push(q14);
+        const q15 = new Question('Christopher Columbus first made landfall in what is now...?', 'Cuba', 'Haiti', 'Puerto Rico', 'The Bahamas', 'd');
+        this.questArr.push(q15);
+        console.log(this.questArr);
     }
 }
     
 $('#a').on('click', () => {
+  game.genQuestions();
   game.begin1P();
 })
 $('#b').on('click', () => {
+  game.genQuestions();
   game.begin2P();
 })
 $('#c').on('click', () => {
+  game.genQuestions();
   game.begin3P();
 })
 $('#d').on('click', () => {
+  game.genQuestions();
   game.begin4P();
 })
 
